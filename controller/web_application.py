8290f29489fa2658dbd1ce349df8b47020de41bd
Yorai <unconfigured@null.spigotmc.org>
2021-05-12 20:46:30 +0300

    Created the model and connected it to the controller

--- controller/web_application.py
+++ controller/web_application.py
@@ -1,5 +1,10 @@
 import os
 from flask import Flask, render_template, request
+import sys
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+from model import anomaly_detection
 
 uploads_folder='controller\\uploads'
 
@@ -12,10 +17,19 @@ def anomaly_detection_web_app():
     return render_template('index.html')
 
 @app.route("/detect", methods=["POST"])
-def detect_anomalies():
-    file = request.files['myfile']
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-    return 'sent ' + file.filename
+def detect():
+    file = request.files['learnFile']
+    learnFile = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+    file.save(learnFile)
+
+    file = request.files['anomaliesFile']
+    anomaliesFile = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+    file.save(anomaliesFile)
+
+    resultsFile = anomaly_detection.detect_anomalies(learnFile, anomaliesFile)
+    print(resultsFile)
+
+    return 'path to learn file: {}<br>path to anomalies file: {}<br>results: {}'.format(learnFile, anomaliesFile, resultsFile)
 
 if __name__ == '__main__':
     app.run(port=8080)
---
+++ model/anomaly_detection.py
@@ -0,0 +1,2 @@
+def detect_anomalies(pathToLearnFile: str, pathToAnomaliesFile: str):
+    return 'path_to_json_file'
