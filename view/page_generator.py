4fe44478258f5f98e1c1598335a9e849845d0e6b
= <dabushori@gmail.com>
2021-05-17 23:49:15 +0300

    Write documentation in README file and added AP2-2.csv file

--- view/page_generator.py
+++ view/page_generator.py
@@ -1,6 +1,4 @@
 from flask import render_template
-from json2html import *
-import json
 import json
 
 def generate_index_page():
@@ -8,10 +6,7 @@ def generate_index_page():
 
 
 def genetrate_results_page(resultsFile: str):
-    print(json2html.convert(resultsFile))
     data = None
     with open(resultsFile) as json_file:
         data = json.load(json_file)
     return render_template('results.html', data=data)
-    # print(data)
-    # return json2html.convert(json=data)
